ba1b57fe327a667528423bb64f3e18ad6fbe9ca3
edumes <lanternaacesatv@gmail.com>
2023-11-17 16:06:56 -0300

    Oxanium font

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -24,7 +24,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
         <title>StarMine: The Cosmic Tycoon</title>
